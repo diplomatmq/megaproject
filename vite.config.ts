a3a05a9210b2ea442cb818214b906d2f558ba901
diplomatmq <diplomatmq@gmail.com>
2026-05-07 04:38:27 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { githubPagesSpa } from '@sctg/vite-plugin-github-pages-spa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), githubPagesSpa()],
+  plugins: [react()],
   base: '/megaproject/',
 })
